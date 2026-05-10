940dc5858881a3d8ba8d1356f8590d2a4ec5680d
camilohl24 <juancamilohernandezhl@gmail.com>
2026-05-10 00:47:50 -0500

    Simple text file

---
+++ UI/Program.cs
@@ -0,0 +1,54 @@
+﻿using Core;
+
+var textFile = new SimpleTextFile("c:\\tmp\\animals.txt");
+var lines = textFile.ReadLines();
+var list = lines.ToList();
+var option = string.Empty;
+
+do
+{
+    option = Menu();
+    switch (option)
+    {
+        case "1":
+            foreach (var item in list)
+            { Console.WriteLine(item); }
+            break;
+        case "2":
+            Console.Write("Enter a new line: ");
+            var newLine = Console.ReadLine();
+            list.Add(newLine!);
+            break;
+        case "3":
+            Console.Write("Enter line to remove: ");
+            var lineToRemove = Console.ReadLine();
+            list.Remove(lineToRemove!);
+            break;
+        case "6":
+            textFile.WriteLines(list.ToArray());
+            Console.WriteLine("Save changes.");
+            break;
+        case "0":
+            Console.WriteLine("Exiting...");
+            break;
+        default:
+            break;
+    }
+
+}
+while (option != "0");
+textFile.WriteLines(list.ToArray());
+Console.WriteLine("Save changes.");
+
+string Menu()
+{
+    Console.WriteLine("1. Show lines");
+    Console.WriteLine("2. Add line");
+    Console.WriteLine("3. Remove one ocurrence");
+    Console.WriteLine("4. Remove all ocurrences");
+    Console.WriteLine("5. Sort");
+    Console.WriteLine("6. Save changes");
+    Console.WriteLine("0. Exit");
+    Console.WriteLine("Choose an option.");
+    return Console.ReadLine() ?? string.Empty;
+}
