40b1b2227bc042896744d8d198b0a7298a198296
camilohl24 <juancamilohernandezhl@gmail.com>
2026-05-10 11:47:25 -0500

    feat: add log

---
+++ Core/LogWriter.cs
@@ -0,0 +1,27 @@
+﻿namespace Core;
+
+public class LogWriter : IDisposable
+{
+    private readonly StreamWriter _writer;
+
+    public LogWriter(string path)
+    {
+        _writer = new StreamWriter(path, append: true)
+        {
+            AutoFlush = true
+        };
+    }
+
+    public void WriteLog(string level, string message)
+    {
+        var timeStamp = DateTime.Now.ToString("s");
+        var logEntry = $"{timeStamp} [{level}] {message}";
+        _writer.WriteLine(logEntry);
+    }
+    public void Dispose()
+    {
+        _writer.Dispose();
+    }
+
+
+}
--- UI/Program.cs
+++ UI/Program.cs
@@ -1,44 +1,64 @@
 ﻿using Core;
 
 var textFile = new SimpleTextFile("c:\\tmp\\animals.txt");
-var lines = textFile.ReadLines();
-var list = lines.ToList();
-var option = string.Empty;
-
-do
+using var logger = new LogWriter("C:\\tmp\\app.log");
+try
 {
-    option = Menu();
-    switch (option)
+    logger.WriteLog("Info", "Application started");
+    var lines = textFile.ReadLines();
+    var list = lines.ToList();
+    var option = string.Empty;
+
+    do
     {
-        case "1":
-            foreach (var item in list)
-            { Console.WriteLine(item); }
-            break;
-        case "2":
-            Console.Write("Enter a new line: ");
-            var newLine = Console.ReadLine();
-            list.Add(newLine!);
-            break;
-        case "3":
-            Console.Write("Enter line to remove: ");
-            var lineToRemove = Console.ReadLine();
-            list.Remove(lineToRemove!);
-            break;
-        case "6":
-            textFile.WriteLines(list.ToArray());
-            Console.WriteLine("Save changes.");
-            break;
-        case "0":
-            Console.WriteLine("Exiting...");
-            break;
-        default:
-            break;
+        option = Menu();
+        switch (option)
+        {
+            case "1":
+                foreach (var item in list)
+                { Console.WriteLine(item); }
+                logger.WriteLog("Info", "File listed");
+                break;
+            case "2":
+                Console.Write("Enter a new line: ");
+                var newLine = Console.ReadLine();
+                list.Add(newLine!);
+                logger.WriteLog("Info", $"New line added: {newLine}");
+                break;
+            case "3":
+                Console.Write("Enter line to remove: ");
+                var lineToRemove = Console.ReadLine();
+                list.Remove(lineToRemove!);
+                logger.WriteLog("Info", $"Line to remove: {lineToRemove}");
+                break;
+            case "6":
+                textFile.WriteLines(list.ToArray());
+                Console.WriteLine("Save changes.");
+                logger.WriteLog("Info", "Lines save");
+                break;
+            case "0":
+                Console.WriteLine("Exiting...");
+                break;
+            default:
+                break;
+        }
+
     }
+    while (option != "0");
+    textFile.WriteLines(list.ToArray());
+    Console.WriteLine("Save changes.");
+
+
 
 }
-while (option != "0");
-textFile.WriteLines(list.ToArray());
-Console.WriteLine("Save changes.");
+catch (Exception ex)
+{
+    logger.WriteLog("Error", $"An error happend: {ex.Message}");
+}
+finally
+{
+    logger.WriteLog("info", "Application ended");
+}
 
 string Menu()
 {
